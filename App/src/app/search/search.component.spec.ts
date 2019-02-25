a80ee050d26b782bce60bed8667ccb200c52304d
Madhu <madhu@itemmaster.com>
2019-02-25 11:01:54 +0530

    [GIFted] Search component  initial bare bone and using in home component

--- App/src/app/search/search.component.spec.ts
+++ App/src/app/search/search.component.spec.ts
@@ -1,23 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { SearchComponent } from './search.component';
 
-
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ],
-      schemas: [NO_ERRORS_SCHEMA]
+      declarations: [ SearchComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
